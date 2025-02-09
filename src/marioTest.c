3bdf88f809073d452004755d93857d6ba6caf43d
PixelCraftedStudios <calebplaysrules@gmail.com>
2025-02-09 19:51:10 +0000

    Update marioTest.c

--- src/marioTest.c
+++ src/marioTest.c
@@ -128,23 +128,3 @@ s32 act_ground_pound(struct MarioState *m) {
 }
 
 
-//     EDIT 09/02/25 - 19:31 GMT  Add the jump code to actually make the gpj work.
-
-s32 act_jump(struct MarioState *m) {
-    
-    hasPerformedGroundPoundJump = 0;
-
-
-    if (check_kick_or_dive_in_air(m)) {
-        return TRUE;
-    }
-
-    if (m->input & INPUT_Z_PRESSED) {
-        return set_mario_action(m, ACT_GROUND_POUND, 0);
-    }
-
-    play_mario_sound(m, SOUND_ACTION_TERRAIN_JUMP, 0);
-    common_air_action_step(m, ACT_JUMP_LAND, MARIO_ANIM_SINGLE_JUMP,
-                           AIR_STEP_CHECK_LEDGE_GRAB | AIR_STEP_CHECK_HANG);
-    return FALSE;
-}
